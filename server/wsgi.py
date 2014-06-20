46178e1bb8e589533786adc536c6a1149ef8c561
Pavel Studeník <studenik@varhoo.cz>
2014-06-21 00:36:26 +0200

    testing on production server

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,9 +1,13 @@
 import sys
 import os
 
-sys.path.insert(0,os.path.dirname(os.path.abspath(__file__)) + '/.')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.production'
 
-import django.core.handlers.wsgi
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/.')
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
 
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
