938fe2fbe52f55a5d45ca4e68d1ffd71d3439ced
Pavel Studenik <studenik@varhoo.cz>
2015-08-01 18:49:13 +0200

    add new signal reciper

---
+++ server/apps/xmlrpc/receiver.py
@@ -0,0 +1,53 @@
+
+import logging
+
+from django.db.models.signals import post_save, pre_delete
+from django.dispatch.dispatcher import receiver
+
+from apps.core.models import Account, ProjectProc, ProjectSetting
+from apps.xmlrpc.models import ActionServer
+
+log = logging.getLogger(__name__)
+
+
+@receiver(pre_delete, sender=Account)
+def account_delete(sender, instance, **kwargs):
+    print "deleting %s" % instance
+
+
+def call(sender, action, instance, **kwargs):
+    name = sender.__name__
+    command = "%s::%s::%s" % (str(name), action, int(instance.id))
+
+    server = {
+        ProjectProc: lambda: instance.project.account.server,
+        ProjectSetting: lambda: instance.account.server,
+        Account: lambda: instance.server
+    }[type(instance)]()
+
+    action = ActionServer(command=command,
+                          command_type=100,
+                          server=server)
+    log.info("Push to queue: %s" % command)
+    action.save()
+
+
+# Account ProjectSetting
+@receiver(post_save, sender=ProjectSetting)
+def project_settings_save(sender, instance, **kwargs):
+    call(sender, "update", instance, **kwargs)
+
+
+# Account ProjectProc
+@receiver(post_save, sender=ProjectProc)
+def project__proc_save(sender, instance, **kwargs):
+    call(sender, "update", instance, **kwargs)
+
+
+# Account signals
+@receiver(post_save, sender=Account)
+def account_save(sender, instance, **kwargs):
+    if kwargs["created"]:
+        call(sender, "create", instance, **kwargs)
+    else:
+        call(sender, "update", instance, **kwargs)
